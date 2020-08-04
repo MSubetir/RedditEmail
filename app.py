4fd19013055e306bb13ec89913408a6eabdfc43f
Maycon Subetir <msubetir@gmail.com>
2020-08-04 09:37:26 -0300

    official

--- app.py
+++ app.py
@@ -39,8 +39,7 @@ def send_email():
     print("Email enviado")
 
 
-schedule.every().minute.do(send_email)
-#schedule.every().day.at("10:00").do(send_email)
+schedule.every().day.at("10:00").do(send_email)
 
 while True:
     schedule.run_pending()
